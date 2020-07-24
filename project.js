5fcc13595ccbe5880f9973dd77705610c6b2e416
kavishakavi <kavishakavi95@gmail.com>
2020-07-24 21:52:26 +0530

    first commit

---
+++ project.js
@@ -0,0 +1,11 @@
+
+
+ function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+ }
+
+ AOS.init();
